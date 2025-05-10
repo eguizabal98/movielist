1a13f0cb002bbdc7ba6115f2eba54521caa79d6e
Elias Eguizabal <elias.eguizabal@gmail.com>
2025-05-09 22:57:04 -0600

    :hammer_and_wrench: Add Retrofit and OkHttp dependencies. Sets up the initial network layer within the data module.

---
+++ data/src/main/java/com/eem/data/api/MoviesApi.kt
@@ -0,0 +1,5 @@
+package com.eem.data.api
+
+interface MoviesApi {
+
+}
---
+++ data/src/main/java/com/eem/data/source/ApiConfig.kt
@@ -0,0 +1,13 @@
+package com.eem.data.source
+
+object ApiConfig {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_READ_TOKEN = "eyJhbGciOiJIUzI1NiJ9." +
+            "eyJhdWQiOiIyNjE1Y2Y2ZTExNzA1ODExMjFkMTZh" +
+            "NmM3ZDdlNGM5ZSIsIm5iZiI6MTYxMjE5NzAyNS45" +
+            "NzMsInN1YiI6IjYwMTgyY2ExODdhMjdhMDA0MGU0" +
+            "OTVjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2" +
+            "ZXJzaW9uIjoxfQ.gBcW0wu_w5-MAcIz8LbWtzwTe" +
+            "hIuAFz_V62D-zphRNE"
+    const val API_KEY = "2615cf6e1170581121d16a6c7d7e4c9e"
+}
