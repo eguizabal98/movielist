1cc075eed212972bb13b1ef8d1f951df548ce565
Elias Eguizabal <elias.eguizabal@gmail.com>
2025-05-09 23:15:14 -0600

    :tada: Setup Data and Domain models for movie list

---
+++ data/src/main/java/com/eem/data/dto/MovieItemDto.kt
@@ -0,0 +1,50 @@
+package com.eem.data.dto
+
+import com.eem.data.util.toImageData
+import com.eem.domain.model.MovieItem
+import com.google.gson.annotations.SerializedName
+
+data class MovieItemDto(
+    @SerializedName("adult")
+    var adult: Boolean? = null,
+    @SerializedName("backdrop_path")
+    var backdropPath: String? = null,
+    @SerializedName("genre_ids")
+    var genreIds: List<Int> = listOf(),
+    @SerializedName("id")
+    var id: Int? = null,
+    @SerializedName("original_language")
+    var originalLanguage: String? = null,
+    @SerializedName("original_title")
+    var originalTitle: String? = null,
+    @SerializedName("overview")
+    var overview: String? = null,
+    @SerializedName("popularity")
+    var popularity: Double? = null,
+    @SerializedName("poster_path")
+    var posterPath: String? = null,
+    @SerializedName("release_date")
+    var releaseDate: String? = null,
+    @SerializedName("title")
+    var title: String? = null,
+    @SerializedName("video")
+    var video: Boolean? = null,
+    @SerializedName("vote_average")
+    var voteAverage: Double? = null,
+    @SerializedName("vote_count")
+    var voteCount: Int? = null
+)
+
+fun MovieItemDto.toDomain(): MovieItem {
+    return MovieItem(
+        id = id ?: 0,
+        originalTitle = originalTitle.orEmpty(),
+        title = title.orEmpty(),
+        backdropUrl = backdropPath.toImageData(),
+        posterUrl = posterPath.toImageData(),
+        overview = overview.orEmpty(),
+        popularity = popularity ?: 0.0,
+        voteAverage = voteAverage ?: 0.0,
+        voteCount = voteCount ?: 0
+    )
+}
---
+++ domain/src/main/java/com/eem/domain/model/MovieItem.kt
@@ -0,0 +1,13 @@
+package com.eem.domain.model
+
+data class MovieItem(
+    var id: Int,
+    var originalTitle: String? = null,
+    var title: String? = null,
+    var backdropUrl: String? = null,
+    var posterUrl: String? = null,
+    var overview: String? = null,
+    var popularity: Double? = null,
+    var voteAverage: Double? = null,
+    var voteCount: Int? = null
+)
