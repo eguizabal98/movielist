2f13a2e724b1f94428049f8a197b3dd7dea62386
Elias Eguizabal <elias.eguizabal@gmail.com>
2025-05-09 22:43:13 -0600

    :hammer_and_wrench: Add OkHttp and Retrofit dependencies

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -43,6 +43,15 @@ dependencies {
     implementation(libs.hilt.core)
     ksp(libs.hilt.compiler)
 
+    // OkHttp
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.logging.interceptor)
+    implementation(libs.okhttp)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
